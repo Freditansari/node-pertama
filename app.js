1a9dea3bbe60b7ed0881a6ec3eb12d1a7efda595
Fredy yudiawan <ftansari@gmail.com>
2019-12-09 15:40:39 +0700

    first-commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express'); //==> beri tahu node js perlu express
+const app = express(); // ==> start express
+const port = process.env.PORT || 3000; //==> deklarasi port yang di buka 3000 atau dideklarasi dari environment variable
+
+
+const patient =[{ //==> deklarasi array object patient
+    name: 'Fredy', 
+    age: 36,
+    address:'Jl. Maju mundur no.20',
+    city: 'Jakarta',
+
+},
+{
+    name: 'Joseph', 
+    age: 40,
+    address:'Jl. Maju mundur no.18',
+    city: 'Jakarta',
+
+},
+
+]
+
+app.get('/patients', (req, res)=>{ //==> deklarasi rest-api end-point patients
+    res.send(patient); //=> kembalikan array object patient
+})
+
+app.get('/', (req, res) => { //==> deklarasi rest-api / (root)
+    res.send('Hello World!'); //==> kembalikan string hello world
+});
+
+app.listen(port, () => { //==> suruh node js untuk mendengarkan port akses sesuai variable port
+    console.log(`hello world app rest-api runs on http://localhost:${port}`);
+});
+
+//Run app, then load http://localhost:port in a browser to see the output.
